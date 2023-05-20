94fdc1e258cb313fb0b26d118b3b5cab6a5aec0c
James Garnfil <jamesgarnfil15@gmail.com>
2023-05-20 22:42:02 +0800

    update mer

--- resources/views/PrintPanel/mer.blade.php
+++ resources/views/PrintPanel/mer.blade.php
@@ -71,7 +71,6 @@
                             </tr>
                         </tbody>
                     </table>
-                    <br>
                     <table width="100%" border="1" cellpadding="0" cellspacing="0">
                         <tbody>
                             <tr>
