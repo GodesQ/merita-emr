6688c21c51bcf35068647ea9bd1218b720840952
Garnfil <jamesgarnfil15@gmail.com>
2024-02-07 09:42:52 +0800

    update follow up

--- resources/views/PrintPanel/follow_up_print.blade.php
+++ resources/views/PrintPanel/follow_up_print.blade.php
@@ -185,7 +185,7 @@
                                             }
                                         }
                                         if(session()->get('employeeId') == 103) {
-                                            dd($findings, $recommendations, $results);
+                                            // dd($findings, $recommendations, $results);
                                         }
                                     @endphp
 
