493c11b11d43fe731e0b796f84e2dabf4bbfd7e0
Garnfil <jamesgarnfil15@gmail.com>
2024-03-06 09:38:52 +0800

    update save_progress_info method

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -252,9 +252,9 @@ class PatientController extends Controller
     public function save_progress_info(Request $request)
     {
         try {
-            // $srb_expdate = $request->srb_expdate ? Carbon::parse($request->srb_expdate)->format('Y-m-d') : date('Y-m-d');
-            // $passport_expdate = $request->passport_expdate ? Carbon::parse($request->passport_expdate)->format('Y-m-d') : date('Y-m-d');
-            // $birthdate = $request->passport_expdate ? Carbon::parse($request->birthdate)->format('Y-m-d') : date('Y-m-d');
+            $srb_expdate = $request->srb_expdate ? \DateTime::createFromFormat('d/m/Y', $request->srb_expdate)->format('Y-m-d') : date('Y-m-d');
+            $passport_expdate = $request->passport_expdate ? \DateTime::createFromFormat('d/m/Y', $request->passport_expdate)->format('Y-m-d') : date('Y-m-d');
+            $birthdate = $request->birthdate ? \DateTime::createFromFormat('d/m/Y', $request->birthdate)->format('Y-m-d') : date('Y-m-d');
 
             $patient_vessel = $request->agency_id == 3 ? $request->bahia_vessel : $request->vessel;
 
