cfc59bb7b855bddaaf99d347581a62e9b74720f4
Garnfil <jamesgarnfil15@gmail.com>
2024-03-06 09:32:20 +0800

    update patient controller

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -252,9 +252,9 @@ class PatientController extends Controller
     public function save_progress_info(Request $request)
     {
         try {
-            $srb_expdate = Carbon::parse($request->srb_expdate ?? date('Y-m-d'))->format('Y-m-d');
-            $passport_expdate = Carbon::parse($request->passport_expdate ?? date('Y-m-d'))->format('Y-m-d');
-            $birthdate = Carbon::parse($request->birthdate ?? date('Y-m-d'))->format('Y-m-d');
+            // $srb_expdate = $request->srb_expdate ? Carbon::parse($request->srb_expdate)->format('Y-m-d') : date('Y-m-d');
+            // $passport_expdate = $request->passport_expdate ? Carbon::parse($request->passport_expdate)->format('Y-m-d') : date('Y-m-d');
+            // $birthdate = $request->passport_expdate ? Carbon::parse($request->birthdate)->format('Y-m-d') : date('Y-m-d');
 
             $patient_vessel = $request->agency_id == 3 ? $request->bahia_vessel : $request->vessel;
 
@@ -291,10 +291,10 @@ class PatientController extends Controller
                 'medical_package' => $request->medicalPackage,
                 'vessel' => strtoupper($patient_vessel),
                 'passportno' => strtoupper($request->passportNo),
-                'passport_expdate' => $passport_expdate,
+                // 'passport_expdate' => $passport_expdate,
                 'srbno' => strtoupper($request->ssrb),
-                'srb_expdate' => $srb_expdate,
-                'birthdate' => $birthdate,
+                // 'srb_expdate' => $srb_expdate,
+                // 'birthdate' => $birthdate,
                 'birthplace' => $request->birthplace,
             ]);
 
