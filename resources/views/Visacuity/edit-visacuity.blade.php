894b80f4773afe29baf5288cee7efeba3f086a4c
Garnfil <jamesgarnfil15@gmail.com>
2025-02-13 13:21:43 +0800

    update the edit form of visual acuity

--- resources/views/Visacuity/edit-visacuity.blade.php
+++ resources/views/Visacuity/edit-visacuity.blade.php
@@ -150,13 +150,11 @@
                                                     <label class="font-weight-bold">Findings</label>
                                                     <textarea class="form-control" name="remarks" id="" cols="30" rows="6">{{ $exam->remarks }}</textarea>
                                                 </div>
-                                                @if (!in_array(session()->get('position'), ["Cashier", "Releasing", "Human Resource / Documentation Officer", "Processing officer", "Information Clerk", "Nurse",  "Asst accounant"]))
-                                                    <div class="form-group">
-                                                        <label class="font-weight-bold">Recommendation</label>
-                                                        <textarea placeholder="Recommendation" class="form-control" name="recommendation" id="" cols="30"
-                                                            rows="6">{{ $exam->recommendation }}</textarea>
-                                                    </div>
-                                                @endif
+                                                <div class="form-group">
+                                                    <label class="font-weight-bold">Recommendation</label>
+                                                    <textarea placeholder="Recommendation" class="form-control" name="recommendation" id="" cols="30"
+                                                        rows="6">{{ $exam->recommendation }}</textarea>
+                                                </div>
                                             </td>
                                         </tr>
                                     </tbody>
@@ -218,3 +216,4 @@
             </div>
         </div>
     @endsection
+                                                     
