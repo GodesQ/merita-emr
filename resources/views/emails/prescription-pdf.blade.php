b0b368468e636d1d6d03521a712061325da387f4
jrmaloc <jhonrobertaloc8@gmail.com>
2025-04-21 14:53:09 +0800

    update prescription pdf

--- resources/views/emails/prescription-pdf.blade.php
+++ resources/views/emails/prescription-pdf.blade.php
@@ -4,33 +4,33 @@
 <head>
     <title>Prescription Slip</title>
     <style>
-    body,
-    table,
-    tr,
-    td {
-        font-family: serif;
-        font-size: 21px;
-        color: green;
-    }
+        body,
+        table,
+        tr,
+        td {
+            font-family: serif;
+            font-size: 20px;
+            color: green;
+        }
 
-    .fontBoldLrg {
-        font: bold 15px sans-serif;
-    }
+        .fontBoldLrg {
+            font: bold 15px sans-serif;
+        }
 
-    .fontMed {
-        font: normal 12px sans-serif;
-    }
+        .fontMed {
+            font: normal 12px sans-serif;
+        }
 
-    .text-below {
-        word-spacing: 1rem;
-        font-weight: 600;
-        font-size: 0.8rem;
-        margin-top: 1rem;
-    }
+        .text-below {
+            word-spacing: 1rem;
+            font-weight: 600;
+            font-size: 0.8rem;
+            margin-top: 1rem;
+        }
 
-    @page {
-        size: letter;
-    }
+        @page {
+            size: letter;
+        }
     </style>
 </head>
 
@@ -39,12 +39,13 @@
         <tbody>
             <tr>
                 <td>
-                    <table width="100%" cellspacing="0" cellpadding="0"  style="margin: 20px 0;">
+                    <table width="100%" cellspacing="0" cellpadding="0" style="margin: 20px 0;">
                         <tbody>
                             <tr>
                                 <td>
                                     <div style='text-align: center;'>
-                                        <h2 style="font-size: 25px; font-weight: 700; line-height: 0px;">MERITA DIAGNOSTIC CLINIC, INC </h2>
+                                        <h2 style="font-size: 25px; font-weight: 700; line-height: 0px;">MERITA
+                                            DIAGNOSTIC CLINIC, INC </h2>
                                         <div>5th Flr. Jettac Bldg. 920 Pres Quirino Ave,</div>
                                         <div>Cor. San Antonio St. Malate Manila</div>
                                         <div>Tel No. (02) 310-05995 / 0917-8576-942</div>
@@ -71,12 +72,13 @@
                                             <tr>
                                                 <td width="17%">Name</td>
                                                 <td style="border-bottom: 1px solid green;" width="83%">
-                                                    <span style="color: black;">{{$patient->firstname}} {{$patient->lastname}}</span>
+                                                    <span style="color: black;">{{ $patient->firstname }}
+                                                        {{ $patient->lastname }}</span>
                                                 </td>
                                             </tr>
                                         </tbody>
                                     </table>
-                                </span>
+                                    </span>
                                 </td>
                                 <td>
                                     <table width="100%" cellspacing="0" cellpadding="0">
@@ -84,7 +86,8 @@
                                             <tr>
                                                 <td width="19%">Date: </td>
                                                 <td style="border-bottom: 1px solid green;" width="81%">
-                                                    <span style="color: black;">{{date_format(new DateTime($data->prescription_date), "F d, Y")}}</span>
+                                                    <span
+                                                        style="color: black;">{{ date_format(new DateTime($data->prescription_date), 'F d, Y') }}</span>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -98,7 +101,7 @@
                                             <tr>
                                                 <td width="14%">Address</td>
                                                 <td style="border-bottom: 1px solid green;" width="86%">
-                                                    <span style="color: black;">{{$patient->address}}</span>
+                                                    <span style="color: black;">{{ $patient->address }}</span>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -110,7 +113,7 @@
                                             <tr>
                                                 <td width="20%">Age</td>
                                                 <td style="border-bottom: 1px solid green;" width="80%">
-                                                    <span style="color: black;">{{$patient->age}}</span>
+                                                    <span style="color: black;">{{ $patient->age }}</span>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -128,8 +131,8 @@
             </tr>
             <tr>
                 <td>
-                    <div style="height: 400px;">
-                        <div style="color: black; margin-left: 80px; font-size: 30px; line-height: 35px;">
+                    <div style="height: 360px;">
+                        <div style="color: black; margin-left: 80px; font-size: 23px; line-height: 30px;">
                             @php echo nl2br($data->prescription) @endphp
                         </div>
                     </div>
@@ -152,14 +155,16 @@
                                             <tr>
                                                 <td width="80%">
                                                     <div>
-                                                        <img src="@php echo $doctor->signature @endphp" style="width: 80%;object-fit:cover;" alt="">
+                                                        <img src="@php echo $doctor->signature @endphp"
+                                                            style="width: 85%;object-fit:cover; transform: translateY(35px);"
+                                                            alt="">
                                                     </div>
                                                     <div
                                                         style="width: 100%; border-bottom: 1px solid green; color: black;">
-                                                        {{$doctor->firstname . " " . $doctor->lastname}}</div>
+                                                        {{ $doctor->firstname . ' ' . $doctor->lastname }}</div>
                                                 </td>
                                                 <td width="20%" style="vertical-align: bottom;">
-                                                    {{$doctor->title}}
+                                                    {{ $doctor->title }}
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -172,7 +177,7 @@
                                                 </td>
                                                 <td width="70%">
                                                     <div style="border-bottom: 1px solid green; color: black;">
-                                                        {{$doctor->license_no}}</div>
+                                                        {{ $doctor->license_no }}</div>
                                                 </td>
                                             </tr>
                                         </tbody>
